de7b39147eda83bb83cd7ce08fe9c276a0aa5a18
Quinn Sissler <QSissler@gmail.com>
2023-05-06 17:38:17 -0400

    commit tree bd10e86cff5bfbaeb5a21830124dd598ef12f7c4

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(array, string){
+    let matching = array.filter(element => element.toLowerCase() === string.toLowerCase());
+    return matching;
+}
+
+function fuzzyMatch(array, string){
+    let matching = array.filter(function (element){
+        return element.charAt(0) === string.charAt(0)
+    })
+    return matching;
+}
+
+function matchName(array, string){
+    let matching = array.filter(object => object.name === string)
+    return matching;
+}
